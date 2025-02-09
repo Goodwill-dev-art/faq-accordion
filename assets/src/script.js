0c7a95e0a2798fb5074b87a38a21660c03778818
Goodwill-dev-art <oriakhig@gmail.com>
2025-02-09 14:28:28 +0100

    set up script for accordion

--- assets/src/script.js
+++ assets/src/script.js
@@ -6,6 +6,29 @@ faqQuestionContainerEl.addEventListener("click", function(e){
          // allArticle.forEach(e=>e.classList.remove("faq__section--answer-active"))
          if (!click) return 
   click.classList.toggle("faq__section--button-minus")
-         document.querySelector(`.faq__section--answer--${click.dataset.tab}`).classList.toggle("faq__section--answer-active")
+         document.querySelector(`.faq__section--answer-${click.dataset.tab}`).classList.toggle("faq__section--answer-active")
 
 })
+// const faqQuestionContainerEl = document.querySelector(".faq__section--container");
+// const allArticle = document.querySelectorAll(".faq__section--answer");
+// const allBtn = document.querySelectorAll(".faq__section--button");
+// let data = "";
+
+// faqQuestionContainerEl.addEventListener("click", function (e) {
+//        const click = e.target.closest(".faq__section--button");
+
+
+
+//        if (!click) return;
+
+
+  
+//        click.classList.toggle("faq__section--button-minus");
+//        document.querySelector(`.faq__section--answer-${click.dataset.tab}`).classList.toggle("faq__section--answer-active");
+//        allBtn.forEach(e => {
+              
+//        })
+
+
+       
+// });
