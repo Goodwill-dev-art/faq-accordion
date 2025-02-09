5102c04a76ce64374a9e44a50cfe20b8c326bc5e
Goodwill-dev-art <oriakhig@gmail.com>
2025-02-09 14:44:14 +0100

    correct imaage error

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,10 +1,13 @@
 const  faqQuestionContainerEl =document.querySelector(".faq__section--container")
 const allArticle = document.querySelectorAll(".faq__section--answer")
+const allBtn = document.querySelectorAll(".faq__section--button")
 faqQuestionContainerEl.addEventListener("click", function(e){
          const click = e.target.closest(".faq__section--button")
          console.log(click.dataset.tab)
-         // allArticle.forEach(e=>e.classList.remove("faq__section--answer-active"))
-         if (!click) return 
+ 
+       if (!click) return
+       allArticle.forEach(e => e.classList.remove("faq__section--answer-active"))
+       allBtn.forEach(e => e.classList.remove("faq__section--button-minus"))
   click.classList.toggle("faq__section--button-minus")
          document.querySelector(`.faq__section--answer-${click.dataset.tab}`).classList.toggle("faq__section--answer-active")
 
